b01a838b2038e15b8bea3c22dbd08dbe3021798a
Tosikko <52299458+ajwadjaved@users.noreply.github.com>
2019-10-02 14:50:07 +0000

    Delete accidentally commited file

--- Extract_News.py
+++
@@ -1,8 +0,0 @@
-# Script to scrap news titles off from tribune, thefinancialdaily, thenews and others
-
-import requests
-from bs4 import BeautifulSoup as bs
-
-def tribune_data:
-        url = https://tribune.com.pk/business/
-        
